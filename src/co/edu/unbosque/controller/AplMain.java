794e1761f81ef32e7f52f188169fdb0e662cc7e0
Pc <bmorenor@unbosque.edu.co>
2021-02-17 20:19:35 -0500

    sysout second

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -3,5 +3,6 @@ package co.edu.unbosque.controller;
 public class AplMain {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.printf("otra vez");
     }
 }
