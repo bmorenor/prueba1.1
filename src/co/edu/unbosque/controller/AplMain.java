0c72f832147e83d1db3ecfa25c38186a925e10b1
Pc <bmorenor@unbosque.edu.co>
2021-02-18 15:29:38 -0500

    ultima prueba

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -4,13 +4,6 @@ public class AplMain {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.printf("otra vez");
-        System.out.println("hola mundo");
-        System.out.printf("otra vez");
-        System.out.println("hola mundo");
-        System.out.printf("otra vez");
-        System.out.println("hola mundo");
-        System.out.printf("otra vez");
-        System.out.println("hola mundo");
-        System.out.printf("otra vez");
+
     }
 }
