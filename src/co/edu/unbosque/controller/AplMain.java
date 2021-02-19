1eaac74a2feb3fb5c16ef8b6fe42eeae9ebe98b0
Pc <bmorenor@unbosque.edu.co>
2021-02-19 09:56:28 -0500

    master

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -3,6 +3,7 @@ package co.edu.unbosque.controller;
 public class AplMain {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hola mundo");
 
 
     }
