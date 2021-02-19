93bec270e2b32348428b14dcde18022d770d48a9
Pc <bmorenor@unbosque.edu.co>
2021-02-19 09:27:53 -0500

    master

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -3,19 +3,7 @@ package co.edu.unbosque.controller;
 public class AplMain {
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.printf("otra vez");
-        System.out.println("andres me cae mal");
-        System.out.println("andres me cae mal x2");
-        System.out.println("andres me cae mal");
-        System.out.println("andres me cae mal x2");
-        System.out.println("andres me cae mal");
-        System.out.println("andres me cae mal x2");
-        System.out.println("andres me cae mal");
-        System.out.println("andres me cae mal x2");
-        System.out.println("sdasdasdadas");
-        System.out.println("sdasdasdadas");
-        System.out.println("sdasdasdadas");
-        System.out.println("sdasdasdadas");
+
 
     }
 }
