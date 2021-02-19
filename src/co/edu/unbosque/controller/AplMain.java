63f234fc6f97109230aa711fc09f618e229fe471
Pc <bmorenor@unbosque.edu.co>
2021-02-19 09:19:11 -0500

    Me gusta la gasolina xd

--- src/co/edu/unbosque/controller/AplMain.java
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -10,6 +10,8 @@ public class AplMain {
         System.out.println("andres me cae mal x2");
         System.out.println("andres me cae mal");
         System.out.println("andres me cae mal x2");
+        System.out.println("andres me cae mal");
+        System.out.println("andres me cae mal x2");
 
     }
 }
